7942429ed282bddf0bc9c2a1edc1e44a1b821e87
Andrest07 <57931603+Andrest07@users.noreply.github.com>
2023-06-02 18:20:25 +1000

    Initial commit

---
+++ src/main/java/com/hammertime/hammertime2/Hammertime2Application.java
@@ -0,0 +1,13 @@
+package com.hammertime.hammertime2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hammertime2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hammertime2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hammertime/hammertime2/Hammertime2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hammertime.hammertime2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hammertime2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
